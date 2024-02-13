5ab264d640d433e54bdd9a6d9f9940dcb8046aad
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-02-13 14:19:50 +0200

    Initial comit

---
+++ Assets/Breakable/glass.gd
@@ -0,0 +1,39 @@
+extends Node3D
+
+@export_flags_3d_physics var fragment_collission_layer:int = 1
+@export_flags_3d_physics var fragment_collission_mask:int = 1
+@export var break_speed:float = 1
+@export var min_frag_lifetime:float = 3
+@export var max_frag_lifetime:float = 5
+@onready var Glass_bottle_fractured = $glass_bottle_fractured
+@onready var breakOrigin = $origin
+@export var shatter:bool = false
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("Test"): #test key is k 
+		#Löydä tähän millä kakka imputilla saat sen toimimaan olkoon se tyyliin tarpeeksi nopea hajoaa tai sitten jos pelaaja releasee seuraava collission hajoaa
+		explode()
+
+
+
+func explode():
+	var parent = get_parent()
+	Glass_bottle_fractured.visible = true
+	queue_free()
+	
+	for child in Glass_bottle_fractured.get_children():
+		if child is MeshInstance3D:
+			var frag:Fragment = preload("res://Assets/Breakable/fractured_glass.tscn").instantiate()
+			frag.init_from_mesh(child)
+			frag.collision_layer = fragment_collission_layer
+			frag.collision_mask = fragment_collission_mask
+			parent.add_child(frag)	
+			#frag.position = parent.position
+			#frag.transform = parent.global_transform
+			frag.linear_velocity = Vector3.ZERO / break_speed
+			frag.lifetime = randf_range(min_frag_lifetime, max_frag_lifetime)
+
+func _on_area_3d_body_entered(body):
+	if shatter:
+		explode()
---
+++ Assets/bellet.gd
@@ -0,0 +1,28 @@
+extends RigidBody3D
+class_name bellet
+
+var is_sticking = false
+
+var velocity = Vector3.ZERO
+func destroy_bellet():
+	queue_free()
+
+
+func _on_bullet_cooldown_timeout():
+	destroy_bellet() # Replace with function body.
+
+func  _ready():
+	set_contact_monitor(true)
+	set_max_contacts_reported(5)
+	set_use_custom_integrator(false)
+
+
+func _physics_process(delta):
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		var collision_point = collision_info.get_position()
+		print("Collission occured at: ", collision_point)
+		set_use_custom_integrator(true)
+		set_linear_velocity (Vector3.ZERO)
+		set_angular_velocity (Vector3.ZERO)
+		
