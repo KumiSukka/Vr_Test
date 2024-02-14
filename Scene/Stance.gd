b179013bea160ed0e5dcc6dc70142b19b13e8f90
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-02-14 13:47:23 +0200

    Seems to work, but ...
    Gets input twice figure why, for examble does it get both controllers same time or somekind of physics delta bug

--- Scene/Stance.gd
+++ Scene/Stance.gd
@@ -1,45 +1,33 @@
 extends XRController3D
 
 @export var ax_input = "ax_button"
-var Pointer : Node3D
-var PickUp : Node3D
-#
-#func _ready():
-	#Pointer = _find_child(self, "FunctionPointer")
-	#PickUp = _find_child(self, "FunctionPickup")
-	#
-#func _process(delta):
-	#if _controller.is_button_pressed(ax_input): #This seems to work for getting input so progress
-		#print("ax_pressed")
+@onready var PickUp = $FunctionPickup
+@onready var Pointer = $FunctionPointer
+
+
+
+
+
+func _ready():
+	print(PickUp)
+	print(Pointer)
+	
+func _process(delta):
+	if is_button_pressed(ax_input): #This seems to work for getting input so progress
+		print("ax_pressed")
+		stanceSwap()
 
 		
-#func stanceSwap():
-	#if (_leftPickUp.enabled == true):
-			#_leftPickUp.enabled = false
-			#_rightPickUp.enabled = false
-			#print("PickUp disabled")
-	#else:
-			#_leftPickUp.enabled = true
-			#_rightPickUp.enabled = true
-			#print("PickUp enabled")
-	#if (_leftPointer.enabled == true):
-		#_leftPointer.enabled = false
-		#_rightPointer.enabled == false
-	#else:
-		#_leftPointer.enabled = true
-		#_rightPointer.enabled == true
+func stanceSwap():
+	if (PickUp.enabled == true):
+			PickUp.enabled = false
+			print("PickUp disabled")
+	else:
+			PickUp.enabled = true
+			print("PickUp enabled")
+	if (Pointer.enabled == true):
+		Pointer.enabled = false
+	else:
+		Pointer.enabled = true
 		
-#static func _find_child(node : Node, type : String) -> Node:
-	## Iterate through all children
-	#for child in node.get_children():
-		## If the child is a match then return it
-		#if child.is_class(type):
-			#return child
-#
-		## Recurse into child
-		#var found := _find_child(child, type)
-		#if found:
-			#return found
-#
-	## No child found matching type
-	#return null
+
