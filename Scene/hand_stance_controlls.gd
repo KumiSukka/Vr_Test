3a5139256d4358fdd7dbcf847a822196cea6e7f2
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-02-14 12:54:45 +0200

    Errors to comment
    evertyghing that prints error to comment for trouble shooting and getting this to work

--- Scene/hand_stance_controlls.gd
+++ Scene/hand_stance_controlls.gd
@@ -1,10 +1,10 @@
 extends XRToolsHand
 
 @export var ax_input = "ax_button"
-@onready var _leftPickUp =  $LeftHand/XRToolsFunctionPickup
-@onready var _leftPointer = $LeftHand/FunctionPointer
-@onready var _rightPointer = $RightHand/FunctionPointer
-@onready var _rightPickUp = $RightHand/XRToolsFunctionPickup
+#@onready var _leftPickUp =  $LeftHand/XRToolsFunctionPickup
+#@onready var _leftPointer = $LeftHand/FunctionPointer
+#@onready var _rightPointer = $RightHand/FunctionPointer
+#@onready var _rightPickUp = $RightHand/XRToolsFunctionPickup
 
 
 # Left controller node
@@ -21,20 +21,19 @@ func _process(delta):
 	if _controller.is_button_pressed(ax_input): #This seems to work for getting input so progress
 		print("ax_pressed")
 		#Simppeli if else pickupin päälle ja pois riippuen onko päällä
-		stanceSwap()
 		
-func stanceSwap():
-	if (_leftPickUp.enabled == true):
-			_leftPickUp.enabled = false
-			_rightPickUp.enabled = false
-			print("PickUp disabled")
-	else:
-			_leftPickUp.enabled = true
-			_rightPickUp.enabled = true
-			print("PickUp enabled")
-	if (_leftPointer.enabled == true):
-		_leftPointer.enabled = false
-		_rightPointer.enabled == false
-	else:
-		_leftPointer.enabled = true
-		_rightPointer.enabled == true
+#func stanceSwap():
+	#if (_leftPickUp.enabled == true):
+			#_leftPickUp.enabled = false
+			#_rightPickUp.enabled = false
+			#print("PickUp disabled")
+	#else:
+			#_leftPickUp.enabled = true
+			#_rightPickUp.enabled = true
+			#print("PickUp enabled")
+	#if (_leftPointer.enabled == true):
+		#_leftPointer.enabled = false
+		#_rightPointer.enabled == false
+	#else:
+		#_leftPointer.enabled = true
+		#_rightPointer.enabled == true
