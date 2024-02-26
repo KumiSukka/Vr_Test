9d521c2bd6df964d861d39a767ba33b2180cba61
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-02-26 08:59:08 +0200

    cooldown on stance swap
    with this we should fix issue with stances swapping constantly

--- Scene/Stance.gd
+++ Scene/Stance.gd
@@ -3,29 +3,42 @@ extends XRController3D
  
 @onready var PickUp = $FunctionPickup
 @onready var Pointer = $FunctionPointer
+@onready var timer = $Timer
 @export var by_input : String = "by_button" #Must be string otherwise takes all inputs for some reason
-
+var stance_cooldown = false
 
 
 		#repeats twice figure out why???
 func stanceSwap():
 	if (PickUp.enabled == true):
+			stance_cooldown = true
 			PickUp.enabled = false
 			PickUp.visible = false
 			print("PickUp disabled")
 	else:
+			stance_cooldown = true
 			PickUp.enabled = true
 			PickUp.visible = true
 			print("PickUp enabled")
 	if (Pointer.enabled == true):
+		stance_cooldown = true
 		Pointer.enabled = false
 		Pointer.visible = false
 	else:
+		stance_cooldown = true
 		Pointer.enabled = true
 		Pointer.visible = true
 		
 
 func _process(delta):
 	if is_button_pressed(by_input):
-		print("by painettu")
-		stanceSwap()
+		if stance_cooldown == true:
+			timer.start()
+		else:
+			print("by painettu")
+			stanceSwap()
+
+
+#With this timer we should get short cooldown with swapping stances hopefully a fixt to it spamming stances
+func _on_timer_timeout():
+	stance_cooldown = false
