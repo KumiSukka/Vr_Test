64aa58fb1de4e58275776e8034bdfbad1aa95c63
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-02-26 08:43:53 +0200

    napit toimivat testi menussa

--- Scene/menu.gd
+++ Scene/menu.gd
@@ -3,3 +3,11 @@ extends CanvasLayer
 
 func _on_button_button_down():
 	print("Painoit nappia")
+
+
+func _on_button_2_button_down():
+	print("Painoit nappia")
+
+
+func _on_button_3_button_down():
+	print("Painoit nappia")
