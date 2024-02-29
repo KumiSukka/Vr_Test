6dd5e0ce785f18cefc900a8ecb301358c4765fde
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-02-29 09:25:49 +0200

    Scene vaihto
    myöskin uusi viewportti

--- Scene/menu.gd
+++ Scene/menu.gd
@@ -1,5 +1,9 @@
 extends CanvasLayer
 
 
-func _on_button_button_down():
-	print("Painoit nappia")
+
+func _on_button_pressed():
+	_change_to_range()
+
+func _change_to_range():
+	get_tree().change_scene_to_file("res://Scene/shooting_range.tscn")
