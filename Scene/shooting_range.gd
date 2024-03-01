ac0be3e69cd3a682e6be48d6755a46963323f363
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-03-01 12:53:48 +0200

    catching player and changing checkpoint
    simple area detection for checkpoint platforms to make game easier especially for testing

--- Scene/shooting_range.gd
+++ Scene/shooting_range.gd
@@ -3,6 +3,10 @@ extends Node3D
 
 @onready var player = $XROrigin3D
 @onready var returnpoint = $Checkpoint
+@onready var returnpoint2 = $Checkpoint2
+@onready var returnpoint3 = $Checkpoint3
+
+var current_return
 var xr_interface: XRInterface
 
 
@@ -20,14 +24,32 @@ func _ready():
 		get_viewport().use_xr = true
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
+	current_return = returnpoint.global_position
 
 
 
 func _on_fall_catcher_body_entered(body):
 	print(body)
-	player.global_position = returnpoint.global_position
+	print(current_return)
+	player.global_position = current_return.global_position
 
 
 func _on_fall_catcher_body_exited(body):
 	print(body)
-	player.global_position = returnpoint.global_position
+	player.global_position = current_return.global_position
+
+
+func _on_detect_player_body_entered(body):
+	current_return = returnpoint.global_position
+	print(current_return)
+	
+
+
+func _on_detect_player_2_body_entered(body):
+	current_return = returnpoint2.global_position
+	print(current_return)
+
+
+func _on_detect_player_3_body_entered(body):
+	current_return = returnpoint3.global_position
+	print(current_return)
