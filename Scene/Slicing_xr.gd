62e84f050fea88d3414459a1fc80d3f624fa07ad
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-03-08 10:40:55 +0200

    first_mesh_slicing test
    working slicing

--- Scene/Slicing_xr.gd
+++ Scene/Slicing_xr.gd
@@ -4,8 +4,8 @@ extends Node3D
 @onready var player = $XROrigin3D
 
 var mesh_slicer = MeshSlicer.new()
+var CrossSection = load("res://Cross_Section.tres")
 var xr_interface: XRInterface
-
 
 #Must run at stat to get xr point in godot
 func _ready():
@@ -22,3 +22,21 @@ func _ready():
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
 
+func  slice():
+	var Transform = $MeshInstance3D.global_transform
+	var MeshInstance = $MeshInstance3D2
+	
+	Transform.origin = $MeshInstance3D2.to_local(Transform.origin)
+	Transform.basis.x = $MeshInstance3D2.to_local(Transform.basis.x + MeshInstance.global_position)
+	Transform.basis.y = $MeshInstance3D2.to_local(Transform.basis.y + MeshInstance.global_position)
+	Transform.basis.z = $MeshInstance3D2.to_local(Transform.basis.z + MeshInstance.global_position)
+	var meshes = mesh_slicer.slice_mesh(Transform,MeshInstance.mesh, CrossSection)
+	$MeshInstance3D3.visible = true
+	
+	MeshInstance.mesh = meshes[0]
+	$MeshInstance3D3.mesh = meshes[1]
+
+
+func _on_timer_timeout():
+	slice()
+	
