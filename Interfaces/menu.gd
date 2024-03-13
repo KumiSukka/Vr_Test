e6a0fcb6ee67b6196aa4a2351ae67f675fbf1096
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-03-13 13:10:17 +0200

    valot

--- Interfaces/menu.gd
+++ Interfaces/menu.gd
@@ -2,22 +2,17 @@ extends CanvasLayer
 
 
 
-func _on_button_pressed():
-	_change_to_range()
-
-func _change_to_range():
-	print("tp_to_range")
+func _on_but_1_pressed():
 	get_tree().change_scene_to_file("res://Scene/shooting_range.tscn")
 
 
-func _on_button_2_pressed():
-	print("tp_to_climbing")
+func _on_but_2_pressed():
 	get_tree().change_scene_to_file("res://Scene/Climbing_test.tscn")
 
 
-func _on_button_3_pressed():
+func _on_but_3_pressed():
 	get_tree().change_scene_to_file("res://Scene/mini_golf.tscn")
 
 
-func _on__pressed():
+func _on_but_4_pressed():
 	get_tree().change_scene_to_file("res://Scene/mesh_slicing.tscn")
