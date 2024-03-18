da5f9dcd4f1b08aa5f206974235d29bc1f710846
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-03-18 16:56:44 +0200

    keila peli logiikka ja kartta update

--- Scene/Pins.gd
+++ Scene/Pins.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
-
+@onready var timer: Timer = $"../Reset_position"
+@onready var monitor: Area3D = $"../Detect_Ball"
 var array_position = []
 var child_lenght = 0
+var _reset = false
 func _ready():
 	for n in self.get_children():
 		array_position.append(n.global_position)
@@ -19,6 +21,20 @@ func _physics_process(delta):
 		for n in self.get_children():
 			n.freeze = true
 		reset()
-		
+	if _reset == true:
+		for n in self.get_children():
+			n.freeze = true
+		reset()
+		_reset = false
+		monitor.monitoring = true
 	else :
 		pass
+
+
+func _on_reset_position_timeout():
+	_reset = true
+
+
+func _on_detect_ball_body_entered(body):
+	timer.start()
+	monitor.monitoring = false
