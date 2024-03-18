238c321ccfed64845c0c0570f3f0a5d4ff64303e
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-03-18 14:47:52 +0200

    functionality to reset bowling pins made

--- Scene/Pins.gd
+++ Scene/Pins.gd
@@ -13,11 +13,14 @@ func reset():
 		var positionZero: Vector3 =  array_position[child_lenght]
 		n.position = positionZero
 		n.rotation=Vector3.ZERO
-		n.set_velocity(Vector3.ZERO)
 		child_lenght += 1
+		n.freeze = false
 	child_lenght = 0
 func _physics_process(delta):
 	if Input.is_action_just_pressed("key_pressed"):
+		for n in self.get_children():
+			n.freeze = true
 		reset()
+		
 	else :
 		pass
