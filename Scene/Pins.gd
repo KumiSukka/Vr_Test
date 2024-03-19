4c624fbe8896c1400f057d17d67ba623f9c50af9
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-03-19 10:33:31 +0200

    radian math for falling

--- Scene/Pins.gd
+++ Scene/Pins.gd
@@ -2,13 +2,17 @@ extends Node3D
 
 @onready var timer: Timer = $"../Reset_position"
 @onready var monitor: Area3D = $"../Detect_Ball"
+
 var array_position = []
+var array_rotation = []
 var child_lenght = 0
+var fallen_lenght = 0
 var _reset = false
 var oddnumber = false
 func _ready():
 	for n in self.get_children():
 		array_position.append(n.global_position)
+		array_rotation.append(n.global_rotation)
 func reset():
 	for n in self.get_children():
 		var positionZero: Vector3 =  array_position[child_lenght]
@@ -17,6 +21,11 @@ func reset():
 		child_lenght += 1
 		n.freeze = false
 	child_lenght = 0
+func hasfallen():
+	for n in self.get_children():
+		if (n.global_rotation.x >  0.7853981634) || (n.global_rotation.z >  0.7853981634) || (n.global_rotation.z <= -0.7853981634) || (n.global_rotation.x <= -0.7853981634):
+			print("has fallen: ", n.rotation)
+			fallen_lenght += 1
 func _physics_process(delta):
 	if Input.is_action_just_pressed("key_pressed"):
 		for n in self.get_children():
@@ -34,6 +43,7 @@ func _physics_process(delta):
 
 
 func _on_reset_position_timeout():
+	hasfallen()
 	if oddnumber == true:
 		_reset = true
 	else:
