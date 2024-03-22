030669b6d1bfb043ba1d7c0a08b4fadc532e3a4e
Kumi <38704075+KumiSukka@users.noreply.github.com>
2024-03-22 10:52:39 +0200

    working for floater + boat physics with water

---
+++ floater.gd
@@ -0,0 +1,22 @@
+extends XRToolsPickable
+
+@export var float_force = 1.0
+@export var water_drag = 0.05
+@export var water_angular_drag = 0.05
+
+@onready var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+const water_height = 0.4
+
+var submerged = false
+
+func _physics_process(delta):
+	submerged = false
+	var depth = water_height - global_position.y
+	if depth > water_height:
+		submerged = true
+		apply_central_force(Vector3.UP * float_force * gravity * depth)
+
+func _integrate_forces(state):
+	if submerged:
+		state.linear_velocity *= 1 - water_drag
